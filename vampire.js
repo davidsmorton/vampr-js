5be09e339b91ec71793c8bbf3097d6e9a1cab0ca
davidsmorton <davidsmorton@gmail.com>
2020-10-03 04:50:15 +0000

    recommit from my gist

--- vampire.js
+++ vampire.js
@@ -10,39 +10,33 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVamps = 0;
+    let currentVamp = this;
 
+    // climb "up" the tree (using iteration), counting nodes, until no boss is found
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      numberOfVamps++;
+    }
+
+    return numberOfVamps;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
-  }
-
-  /** Tree traversal methods **/
-
-  // Returns the vampire object with that name, or null if no vampire exists with that name
-  vampireWithName(name) {
-    
-  }
-
-  // Returns the total number of vampires that exist
-  get totalDescendents() {
-    
-  }
-
-  // Returns an array of all the vampires that were converted after 1980
-  get allMillennialVampires() {
-    
+    return (this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal)
+      
   }
 
   /** Stretch **/
